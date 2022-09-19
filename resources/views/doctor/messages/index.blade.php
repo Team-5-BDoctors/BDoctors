12dd3c9ac5f70e750575062d73e52089c21b40be
Minino99 <loizzo.pasqualino@gmail.com>
2022-09-19 12:12:25 +0200

    created index messages

--- resources/views/doctor/messages/index.blade.php
+++ resources/views/doctor/messages/index.blade.php
@@ -1,1 +1,43 @@
-<h1>pagina messaggi ricevuti</h1>
+@extends("layouts.app")
+
+@section("content")
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">{{ __("Messaggi") }}</div>
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col-12">
+                                <div class="table-responsive">
+                                    <table class="table table-striped">
+                                        <thead>
+                                            <tr>
+                                                <th scope="col">Nome</th>
+                                                <th scope="col">Cognome</th>
+                                                <th scope="col">Email</th>
+                                                <th scope="col">Messaggio</th>
+                                                <th scope="col">Data</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            @foreach ($messages as $message)
+                                                <tr>
+                                                    <td>{{ $message->name }}</td>
+                                                    <td>{{ $message->surname }}</td>
+                                                    <td>{{ $message->email }}</td>
+                                                    <td>{{ $message->message }}</td>
+                                                    <td>{{ $message->created_at }}</td>
+                                                </tr>
+                                            @endforeach
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
