486d27f7a1f010edcf8bc3a0a1f9c97679e7d825
Luketto-dev <luca.francesconi1995@gmail.com>
2022-09-20 18:04:07 +0200

    modify form-style in doctor.edit

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -36,17 +36,14 @@
                         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                             <span class="navbar-toggler-icon"></span>
                         </button>
-            
                         <div class="collapse navbar-collapse" id="navbarSupportedContent">
                             <!-- Left Side Of Navbar -->
                             <ul class="navbar-nav mr-auto">
             
                             </ul>
-            
                             <!-- Right Side Of Navbar -->
                             <ul class="navbar-nav ml-auto">
                                 <!-- Authentication Links -->
-                                
                                     <li class="nav-item">
                                         <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
                                     </li>
@@ -55,7 +52,6 @@
                                             <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
                                         </li>
                                     @endif
-                                
                             </ul>
                         </div>
                     </div>
